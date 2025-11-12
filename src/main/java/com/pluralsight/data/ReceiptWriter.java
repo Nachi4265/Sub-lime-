27eb9cd4e6f6c8c6a2c601f93a719389e34406cb
Nachi4265 <hvirgen@my.yearupunited.org>
2025-11-11 20:42:24 -0500

    commit

--- src/main/java/com/pluralsight/data/ReceiptWriter.java
+++ src/main/java/com/pluralsight/data/ReceiptWriter.java
@@ -1,4 +1,23 @@
 package com.pluralsight.data;
 
 public class ReceiptWriter {
+    /*Phase 5: Receipt System
+Goal: Save receipts to files
+
+Implement ReceiptWriter
+
+
+Method to generate filename from date/time
+Method to create receipts folder
+Method to write order details to file
+
+
+Connect Receipt to Checkout
+
+
+When user confirms, call ReceiptWriter
+Save the receipt
+Return to home screen*/
+
+
 }
